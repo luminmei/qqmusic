113893d8ab48ec645a3416fd9cb56de3a4acb270
luminmei <514911632@qq.com>
2019-02-15 22:14:26 +0800

    页面基本样式

---
+++ dist/js/index.js
@@ -0,0 +1,14 @@
+function getData(url) {
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: function (data) {
+      console.log(data)
+    },
+    error: function () {
+      console.log(error)
+    }
+  })
+}
+
+getData ("../mock/data.json")
