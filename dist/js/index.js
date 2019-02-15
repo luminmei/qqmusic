fef2ee745b7d5b6032ef7d8a106cd08ee957d8bd
luminmei <514911632@qq.com>
2019-02-15 23:27:37 +0800

    前后按钮

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,43 @@
 var root = window.player
-
+var nowIndex = 0
+var dataList
+var len 
 function getData(url) {
   $.ajax({
     type: 'GET',
     url: url,
-    success: function (data) {
-      console.log(data)
-      root.render(data[0])
+    success: function (data) {   
+      dataList = data
+      len = dataList.length
+      root.render(dataList[0])
+      bindEvent()
+      console.log(dataList)
     },
     error: function () {
       console.log(error)
     }
   })
 }
+
+function bindEvent() {
+  $('.prev').on('click', function () {
+    if (nowIndex == 0) {
+      nowIndex = len - 1
+    } else {
+      nowIndex--
+    }
+    root.render(dataList[nowIndex])
+  })
+  $('.next').on('click', function () {
+    if (nowIndex == len - 1) {
+      nowIndex = 0
+    } else {
+      nowIndex++
+    }
+    root.render(dataList[nowIndex])
+  })
+}
+
 
 getData ("../mock/data.json")
 
