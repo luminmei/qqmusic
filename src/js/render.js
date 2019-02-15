48b91a86be7d05c24ea757d96e0559aef3aa7202
luminmei <514911632@qq.com>
2019-02-15 23:15:53 +0800

    标记喜欢

--- src/js/render.js
+++ src/js/render.js
@@ -10,14 +10,25 @@
     }
   }
   function renderInfo(info) {
-
+    var str = `
+    <div class="song-name">` + info.song + `</div>
+    <!-- 歌手名字 -->
+    <div class="singer-name">` +  info.singer + `</div>
+    <!-- 专辑名字 -->
+    <div class="album-name">` + info.album + `</div>`;
+    $('.song-info').html(str)
   }
-  function renderIsLike() {
-
+  function renderIsLike(like) {
+    if (like) {
+      $('.like').addClass('liking')
+    } else {
+      $('.like').removeClass('liking')
+    }
   }
   root.render = function (data) {
     renderImg(data.image)
-    // renderInfo()
+    renderInfo(data)
+    renderIsLike(data.isLike)
   }
 })(window.Zepto, window.player || (window.player = {}));
 // window.Zepto 在里面本来就可以取到，为了不用每次查找所以传进来了
