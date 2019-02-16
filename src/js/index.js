9ca7a2114525afc4eb8f743237e68b7d2b163812
luminmei <514911632@qq.com>
2019-02-16 10:51:13 +0800

    优化函数

--- src/js/index.js
+++ src/js/index.js
@@ -28,43 +28,49 @@ function getData(url) {
 }
 
 function bindEvent() {
-  console.log('绑定事件')
+  // 自定义事件
+  $('body').on('play:change', function (e, index) { // 第一个参数是自定义事件
+    root.render(dataList[index])
+    audio.getAudio(dataList[index].audio)
+    if (audio.status == 'play') {
+      audio.play()
+    }
+  })
   $('.prev').on('click', function () {
-    console.log('下一首歌')
     // if (nowIndex == 0) {
     //   nowIndex = len - 1
     // } else {
     //   nowIndex--
     // }
     var i = control.prev() // 返回当前的索引值
-    root.render(dataList[i])
-    audio.getAudio(dataList[i].audio)
-    if (audio.status == 'play') {
-      audio.play()
-    }
+    // root.render(dataList[i])
+    // audio.getAudio(dataList[i].audio)
+    // if (audio.status == 'play') {
+    //   audio.play()
+    // }
+    $('body').trigger('play:change', i) // 触发自定义事件
   })
   $('.next').on('click', function () {
-    console.log('下一首歌')
     // if (nowIndex == len - 1) {
     //   nowIndex = 0
     // } else {
     //   nowIndex++
     // }
     var i = control.next() // 返回当前的索引值
-    root.render(dataList[i])
-    audio.getAudio(dataList[i].audio)
-    if (audio.status == 'play') { // 为了在切换歌曲的时候，播放就继续播放
-      audio.play()
-    }
+    // root.render(dataList[i])
+    // audio.getAudio(dataList[i].audio)
+    // if (audio.status == 'play') { // 为了在切换歌曲的时候，播放就继续播放
+    //   audio.play()
+    // }
+    $('body').trigger('play:change', i)
   })
   $('.play').on('click', function () {
-    console.log('切换播放状态')
     if (audio.status == 'pause') {
       audio.play()
     } else {
       audio.pause()
     }
-    $('.play').toggleClass('playing')
+    $('.play').toggleClass('playing') // 切换按钮的图片
   })
 }
 
@@ -75,6 +81,8 @@ getData ("../mock/data.json")
 // 信息+图片渲染到页面上  render
 // 点击按钮
 // 音频的播放与暂停 切割
-// 进度条运动与拖拽
 // 图片旋转
+
+
 // 列表切歌
+// 进度条运动与拖拽
